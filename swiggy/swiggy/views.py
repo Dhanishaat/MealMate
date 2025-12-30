e5e08d7afd239dca90b93743dbccff4d6fec2aa4
Dhanishaat <dhanishamannur@gmail.com>
2025-12-31 01:01:46 +0530

    initial commit

---
+++ swiggy/swiggy/views.py
@@ -0,0 +1,5 @@
+# from django.http import HttpResponse
+# def hello(request):
+#     return HttpResponse("Hello, welcome to django")
+# def thankyou(request):
+#     return HttpResponse("Thankyou django")
