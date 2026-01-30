630dbab65bd8d57850245b6cd89bb715240df83b
Dhanishaat <dhanishamannur@gmail.com>
2026-01-30 21:25:02 +0530

    settings change

--- swiggy/swiggy/settings.py
+++ swiggy/swiggy/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
 RAZORPAY_KEY_ID = 'rzp_test_Rxw690RKv4qkM3'
